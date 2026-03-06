60c6fd99f1b2f1db8e6df7613cededd05f4c0862
Vova Polyakov <vova_polyakov_94@mail.ru>
2026-03-06 11:55:56 +0300

    fix vercel build folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import legacy from "@vitejs/plugin-legacy"
 
 export default defineConfig({
   base: "./",
@@ -13,6 +13,6 @@ export default defineConfig({
   ],
 
   build: {
-    outDir: "dist"
+    outDir: "build"
   }
-});
+})
