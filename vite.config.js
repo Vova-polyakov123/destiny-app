de616624e40597aa819d328dc9445e6e24e98cbe
Vova Polyakov <vova_polyakov_94@mail.ru>
2026-03-06 11:45:23 +0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,15 @@
-import { defineConfig, transformWithEsbuild } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 
-function handleModuleDirectivesPlugin() {
-  return {
-    name: "handle-module-directives-plugin",
-    transform(code, id) {
-      if (id.includes("@vkontakte/icons")) {
-        code = code.replace(/"use-client";?/g, "");
-      }
-      return { code };
-    },
-  };
-}
-
-function treatJsFilesAsJsx() {
-  return {
-    name: "treat-js-files-as-jsx",
-    async transform(code, id) {
-      if (!id.match(/src\/.*\.js$/)) return null;
-
-      return transformWithEsbuild(code, id, {
-        loader: "jsx",
-        jsx: "automatic",
-      });
-    },
-  };
-}
-
 export default defineConfig({
   base: "./",
-
   plugins: [
     react(),
-    treatJsFilesAsJsx(),
-    handleModuleDirectivesPlugin(),
     legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
+      targets: ["defaults", "not IE 11"]
+    })
   ],
-
-  optimizeDeps: {
-    force: true,
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-      },
-    },
-  },
-
   build: {
     outDir: "dist"
   }
