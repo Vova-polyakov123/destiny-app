b6006ab2f7c2093e6fcba03806a8ceabb415dea3
Vova Polyakov <vova_polyakov_94@mail.ru>
2026-03-06 11:49:32 +0300

    fix vercel build

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   base: "./",
+
   plugins: [
     react(),
     legacy({
       targets: ["defaults", "not IE 11"]
     })
   ],
+
   build: {
     outDir: "dist"
   }
