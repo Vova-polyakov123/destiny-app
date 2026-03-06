cc64829d6704b302818d6da6cf95bd69b6ca497b
Vova Polyakov <vova_polyakov_94@mail.ru>
2026-03-06 12:34:51 +0300

    fix build

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div style={{ textAlign: "center", marginTop: "100px" }}>
+            <h1>Destiny App</h1>
+            <p>Приложение работает 🚀</p>
+        </div>
+    );
+}
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import legacy from "@vitejs/plugin-legacy"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",
-
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults", "not IE 11"]
-    })
-  ],
-
+  plugins: [react()],
   build: {
-    outDir: "dist"
+    outDir: "build"
   }
 })
